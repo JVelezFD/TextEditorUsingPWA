6e5c593c278e14c0dbe616c3897f9d80ef58b734
JVelezFD <jvelez117@gmail.com>
2022-09-15 15:21:24 -0400

    added logic to the db and install js

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # TextEditorUsingPWA
+
+
+  ## Table of Contents:
+
+  * [Description](#Description)
+
+  * [Motivation](#Motivation)
+
+  * [Technologies](#Technologies)
+
+  * [Contributions](#Contributions)
+
+  * [Deployment](#Deployment)
+
+  * [Email](#Questions)
+
+  * [Github](#Questions)
+
+  ## Description: 
+    This text editor runs in the browser. This app will be a single-page application that meets PWA criteria. It will also feature a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. This will also be able to function offline.
+
+  ## Motivation: 
+  The aim is to allow users to create notes or todos with or without an internet connection so that a user can retrieve them for later use
+  
+  ## Technologies: 
+  * PWA
+  * IndexBD
+  * Babel
+  * JS
+  * CSS & HTML
+
+
+  ## Deployment:
+ 
+  
+
+  ## Screenshots: 
+  
+
+  
+
+  ## Questions:
+
+  [Email](mailto:jvelez117@gmail.com)
+
+  [GitHub](https://github.com/JVelezFD)
+
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+const textDB = await openDB('jate', 1);
+const tx = textDB.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const request = store.put({jate: content});
+const result = await request; 
+
+console.log ('Content Stored in DB', result);
+
+}; console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+const textDB = await openDB ('jate', 1);
+const tx = textDB.transaction('jate', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+const result = await request; 
+
+result ? console.log('All data retrieved') : console.log ('No data found on DB');
+
+return result?.value;
+
+}; console.error('getDb not implemented');
 
 initdb();
